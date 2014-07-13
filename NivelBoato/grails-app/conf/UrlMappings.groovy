bc26114861e7d207ca67c7192bbc7c6efe356333
leonardo <leonardo@leonardo-Casa>
2014-07-13 13:37:59 -0300

    Altera??es no mapeamento das classes de dominio.

--- NivelBoato/grails-app/conf/UrlMappings.groovy
+++ NivelBoato/grails-app/conf/UrlMappings.groovy
@@ -8,6 +8,7 @@ class UrlMappings {
         }
 
         "/index"(view:"/index")
+        "/"(view:"/index")
         "500"(view:'/error')
 	}
 }
--- NivelBoato/grails-app/domain/entidade/Tag.groovy
+++ NivelBoato/grails-app/domain/entidade/Tag.groovy
@@ -2,14 +2,15 @@ package entidade
 
 class Tag {
 
-	String descricao
-	Date dataCadastro
-	boolean status
-	
-	//TODO Criar a Entidade Groovy Usuario
-	//Usuario usuarioCadastro
+    String descricao
+    Date dataCadastro
+    boolean status
 
     static constraints = {
         descricao size: 1..150, blank: false, unique: true
+        status blank: false, unique: true
+        usuarioCadastro blank: true, nullable: true
     }
+    
+    static belongsTo = [usuarioCadastro: Usuario]
 }
